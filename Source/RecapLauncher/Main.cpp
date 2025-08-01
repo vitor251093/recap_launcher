75f877251868278de8c3af3ce9bf47612116fb1c
VitorMM <vitor251093@gmail.com>
2025-08-01 18:29:12 -0300

    Launching server, if possible

--- Source/RecapLauncher/Main.cpp
+++ Source/RecapLauncher/Main.cpp
@@ -7,12 +7,42 @@
 
 static const char* szDll = "RecapHooks.dll";
 static const char* szExeDefault = "Darkspore.exe";
+static const char* szServerExeDefault = "Server/recap_server.exe";
 
 void PrintUsage()
 {
     printf("Usage: RecapLauncher.exe [--help] [--exe <path_to_exe>]\n\n");
     printf("Options:\n");
     printf("--exe  Path to the Darkspore executable file (default: Darkspore.exe)\n");
+}
+
+bool runExeInNewCmdWindow(const char* exePath) {
+    std::string command = "cmd.exe /C start \"\" \"" + std::string(exePath) + "\"";
+
+    STARTUPINFOA si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+
+    BOOL success = CreateProcessA(
+        NULL,
+        (LPSTR)command.c_str(),
+        NULL,
+        NULL,
+        FALSE,
+        CREATE_NO_WINDOW,
+        NULL,
+        NULL,
+        &si,
+        &pi
+    );
+
+    if (success) {
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+        return true;
+    } else {
+        std::cerr << "Failed to launch process. Error: " << GetLastError() << std::endl;
+        return false;
+    }
 }
 
 int CDECL main(int argc, char** argv)
@@ -22,6 +52,7 @@ int CDECL main(int argc, char** argv)
     CHAR szFullExe[1024] = "\0";
     PCHAR pszFileExe = NULL;
     const char* szExe = szExeDefault;
+    const char* szServerExe = szServerExeDefault;
 
     ZeroMemory(&si, sizeof(si));
     ZeroMemory(&pi, sizeof(pi));
@@ -32,6 +63,10 @@ int CDECL main(int argc, char** argv)
         if ((_stricmp(argv[i], "--exe") == 0) && (++i < argc))
         {
             szExe = argv[i];
+        }
+        else if ((_stricmp(argv[i], "--server") == 0) && (++i < argc))
+        {
+            szServerExe = argv[i];
         }
         else if (_stricmp(argv[i], "--help") == 0)
         {
@@ -52,6 +87,9 @@ int CDECL main(int argc, char** argv)
         printf("Darkspore.exe not found.\n");
         return -2;
     }
+
+    // Launch server, if possible
+    runExeInNewCmdWindow(szServerExe);
 
     DWORD dwFlags = CREATE_DEFAULT_ERROR_MODE | CREATE_SUSPENDED;
     if (!DetourCreateProcessWithDllEx(szFullExe, NULL, NULL, NULL, TRUE, dwFlags, NULL, NULL, &si, &pi, szDll, NULL))
